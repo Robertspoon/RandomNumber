a0e37b0c1b9fce627cca7535dcee3c7189294374
Robertspoon <robertthemcjug@gmail.com>
2023-02-01 09:43:44 -0400

    Initial commit

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Random Number generator");
+
+            Random iRandom = new Random();
+            int random = iRandom.Next(1, 4);
+            Console.WriteLine(random);
+
+
+
+            Console.ReadKey(true);
+
+
+        }
+    }
+}
